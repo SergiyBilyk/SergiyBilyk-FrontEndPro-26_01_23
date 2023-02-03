63c0da726023150f5fb2fa29445b717d58d212e1
SergiyBilyk <shivantom7@gmail.com>
2023-02-04 00:44:19 +0200

    Homework 6

---
+++ home-06/index.js
@@ -0,0 +1,8 @@
+const hours = Number(prompt('Напишіть, яка кількість годин?'));
+let result;
+if (hours) {
+    result = hours * 3600;
+    alert(`${result} секунд`);
+} else {
+    alert('Помилка! Введіть число');
+}
