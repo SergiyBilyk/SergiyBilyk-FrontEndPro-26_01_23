5ef7878ac9314f97bc844a7465f2ae3873859eae
SergiyBilyk <shivantom7@gmail.com>
2023-02-06 23:42:33 +0200

    Homework 9

--- home-09/index.js
+++ home-09/index.js
@@ -1,18 +1,16 @@
 let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+console.log(numOrStr);
 
-if(numOrStr === null) {
-console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-console.log(' number is Ba_NaN')
-} else {
-console.log('OK!')
-}
-
-
-switch(numOrStr) {
-    case null:
-
+switch(true) {
+    case numOrStr === null:
+        console.log('ви скасували')
+        break;
+    case numOrStr.trim() === '': 
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
 }
