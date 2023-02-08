c1637d8e9837c27dd86d815ad02ff353684c120a
SergiyBilyk <shivantom7@gmail.com>
2023-02-08 21:39:49 +0200

    Homework 12

---
+++ home-12/index.js
@@ -0,0 +1,16 @@
+const userArr = prompt('Введіть значення масиву через пробіл');
+const arr = userArr.split(' ');
+const newArr = arr.filter(function(el) {
+    return el !=''
+});
+
+if ( !isNaN(newArr[1])) {
+    newArr.sort((a,b) => a-b);
+    alert(newArr)
+} else {
+    newArr.sort();
+    alert(newArr)
+};
+
+
+
