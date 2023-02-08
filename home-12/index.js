f1e5d5c25beae32370a56526e62ccc1f74c133cc
SergiyBilyk <shivantom7@gmail.com>
2023-02-08 21:50:35 +0200

    Homework 12 new

--- home-12/index.js
+++ home-12/index.js
@@ -3,7 +3,6 @@ const arr = userArr.split(' ');
 const newArr = arr.filter(function(el) {
     return el !=''
 });
-
 if ( !isNaN(newArr[1])) {
     newArr.sort((a,b) => a-b);
     alert(newArr)
@@ -11,6 +10,5 @@ if ( !isNaN(newArr[1])) {
     newArr.sort();
     alert(newArr)
 };
-
-
-
+newArr.splice(1, 3)
+alert(newArr)
