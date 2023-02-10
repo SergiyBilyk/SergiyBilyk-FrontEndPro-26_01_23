2aff3f41f458c5e2ca9ce4db66289ddc9bc80ea6
SergiyBilyk <shivantom7@gmail.com>
2023-02-10 16:57:21 +0200

    Homework 15

---
+++ home-15/index.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+    return array.splice(item - 1, 1);
+}   
+
+removeElement(array, 5);
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
